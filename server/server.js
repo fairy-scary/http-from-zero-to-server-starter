79fc468c92313c4f43ee80bae82a7945f19553bd
bryan <bryan.guner@gmail.com>
2020-09-14 17:22:54 -0400

    addedconditionals

--- server/server.js
+++ server/server.js
@@ -1,10 +1,67 @@
-const http = require('http');
+const http = require( 'http' );
 
 http
-    .createServer(function(req, res) {
+    .createServer( function ( req, res ) {
+        //res.writeHead( 200 );
+        res.end();
+    } )
 
-    })
+    .listen( 3000, function () {
+        console.log( "listening for port 3000" );
+    } )
 
-    .listen(3000, function() {
-        console.log("listening for port 3000");
-    })
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
+
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
+
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
+
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
+
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
+
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
+
+if ( req.url === "/" ) {
+    res.writeHead( , {
+        "Content-Type": "text/html"
+    } );
+    res.write( "<h1></h1>" );
+    res.end();
+}
